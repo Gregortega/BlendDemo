477e5ce77dd04e54803315b22bdb04b7694521af
Greg Ortega <Gregorioortegac@gmail.com>
2017-12-22 17:43:50 +1100

    initial commit

---
+++ BlendDemo/BlendDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlendDemo
+//
+//  Created by Greg Ortega on 22/12/17.
+//  Copyright © 2017 Gregorio Ortega Calderon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
